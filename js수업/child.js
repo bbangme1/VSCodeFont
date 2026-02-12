44a03d31e7b9a0b79ab9a720b49e7b5f9003a174
bbangme1 <bbangme10@gmail.com>
2026-02-12 09:51:52 +0900

    26.02.12

--- js수업/child.js
+++ js수업/child.js
@@ -1,4 +1,35 @@
 
+// 자바스크립트 객체
+// 객체 리터럴
+let obj1 = { name : "이순신" , age : 34 , address : "통영시" };
+obj1.name = "이성계";
+boj1.age;
+obj1.address;
+
+// 함수형 객체
+function user(name, age, address){
+    this.name = name;
+    this.age = age;
+    this.address = address;
+}
+
+let u = new user ("장보고",54,"완도");
+let u1 = new user ("장영실",44,"한양");
+
+// Object() 를 이용하여 객체 만들기
+let u2 = new Object();
+
+// class 문법으로 객체 만들기
+class member{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+// 프로토타입 을 이용한 객체 생성
+boj1.prototype.job="군인";
+
 // 부모창에 입력한 이름을 가져와서 자식창의 input태그에 넣기
 // 새창을 열어줘 부모창(메인창)은 window.opener 이다.
 let parentName = window.opener.document.getElementById("name");
